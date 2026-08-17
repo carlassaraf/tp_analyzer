b4e5f83f15cf22c50d5b316dcd22f9f3fdfbd525
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-08-17 15:03:47 -0300

    feat(adc): add dedicated ADC thread to poll messages from DMA

--- zephyr-workspace/app/src/threads/ad_thread.c
+++ zephyr-workspace/app/src/threads/ad_thread.c
@@ -1,35 +1,40 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include "ad_thread.h"
-// #include "hal/hal_adc.h"
-// #include "lvgl/screen_update.h"
-// #include "board_config.h"
+#include "adc_stream.h"
 
-/** 
- * @brief Called by the DMA ISR in the ADC HAL
- * @param buf Pointer to the DMA buffer with the sampled data
- */
-static void adc_ready_cb(const uint16_t *buf)
-{
-  // BaseType_t woken = pdFALSE;
-  // xTaskNotifyFromISR(s_task_handle, (uint32_t)buf, eSetValueWithOverwrite, &woken);
-  // portYIELD_FROM_ISR(woken);
-}
+/* Custom ADC device from devicetree */
+static const struct device *adc = DEVICE_DT_GET(DT_NODELABEL(adc_stream));
+/* Message queue used to get data from DMA */
+K_MSGQ_DEFINE(adc_msgq, sizeof(struct adc_stream_block), 2 * DT_CHILD_NUM_STATUS_OKAY(DT_NODELABEL(adc_stream)), sizeof(void *));
 
+LOG_MODULE_REGISTER(ad_thread, LOG_LEVEL_INF);
+
+/** @brief AD consumer thread */
 void ad_thread(void *param1, void *param2, void *param3)
 {
   ARG_UNUSED(param1);
   ARG_UNUSED(param2);
   ARG_UNUSED(param3);
-  // s_task_handle = xTaskGetCurrentTaskHandle();
 
-  // hal_adc_init(ADC_SAMPLE_RATE, ADC_GPIO, ADC_CHANNEL);
-  // hal_adc_set_ready_cb(adc_ready_cb);
-  // hal_adc_start();
+	if (!device_is_ready(adc)) {
+		LOG_ERR("ADC stream device not ready");
+		return;
+	}
+
+	int err = adc_stream_start(adc, &adc_msgq);
+	if (err) {
+		LOG_ERR("adc_stream_start failed: %d", err);
+		return;
+	}
+	LOG_INF("ADC stream started, waiting for blocks...");
 
   while (1) {
-    // uint32_t data;
-    // xTaskNotifyWait(0, ULONG_MAX, &data, portMAX_DELAY);
-    // screen_update_cmd_push(SCREEN_UPDATE_OSC_DATA, (void*)data);
-    // screen_update_cmd_push(SCREEN_UPDATE_FFT_DATA,  (void*)data);
+		struct adc_stream_block block;
+		err = k_msgq_get(&adc_msgq, &block, K_SECONDS(2));
+		if (err) {
+			LOG_WRN("No ADC block in 2s - check DMA wiring/pinctrl");
+			continue;
+		}
   }
 }
