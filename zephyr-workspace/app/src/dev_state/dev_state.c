a2b9acc3a4327abf39a2f5efb124eca44a59d4c4
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-08-26 16:16:07 -0300

    feat(timeout): add dev_state owned timers for timeouts

--- zephyr-workspace/app/src/dev_state/dev_state.c
+++ zephyr-workspace/app/src/dev_state/dev_state.c
@@ -1,9 +1,24 @@
 #include "dev_state.h"
 
+// Timeout timers
+static void screen_timeout_expiry(struct k_timer *timer_id);
+static void off_timeout_expiry(struct k_timer *timer_id);
+
+static K_TIMER_DEFINE(screen_timeout_timer, screen_timeout_expiry, NULL);
+static K_TIMER_DEFINE(off_timeout_timer, off_timeout_expiry, NULL);
 static K_MUTEX_DEFINE(s_mux);
 
 // Consistent device state in runtime
 static dev_state_t s_device_state = {0};
+
+// Registered by whoever decides what a timeout means (the UI layer)
+static dev_state_timeout_cb_t s_timeout_cb = NULL;
+
+// Converts a stored ms value to a relative k_timeout_t, 0 meaning "never expire"
+static inline k_timeout_t ms_to_timeout(uint32_t ms)
+{
+  return (ms == 0) ? K_FOREVER : K_MSEC(ms);
+}
 
 bool dev_state_init(void)
 {
@@ -13,11 +28,19 @@ bool dev_state_init(void)
 
   // Load default state
   k_mutex_lock(&s_mux, K_FOREVER);
-  s_device_state.off_screen_timeout_ms = 60000;
   s_device_state.screen_timeout_ms = 30000;
+  s_device_state.off_screen_timeout_ms = 60000;
   k_mutex_unlock(&s_mux);
 
+  // Arm both timers with the loaded values
+  dev_state_kick_activity();
+
   return true;
+}
+
+void dev_state_set_timeout_cb(dev_state_timeout_cb_t cb)
+{
+  s_timeout_cb = cb;
 }
 
 bool dev_state_get(dev_state_t *dev_state)
@@ -32,7 +55,10 @@ bool dev_state_set_off_timeout(uint32_t timeout)
 {
   k_mutex_lock(&s_mux, K_FOREVER);
   s_device_state.off_screen_timeout_ms = timeout;
+  /** @todo Persist to Flash */
   k_mutex_unlock(&s_mux);
+
+  k_timer_start(&off_timeout_timer, ms_to_timeout(timeout), K_NO_WAIT);
   return true;
 }
 
@@ -40,6 +66,37 @@ bool dev_state_set_screen_timeout(uint32_t timeout)
 {
   k_mutex_lock(&s_mux, K_FOREVER);
   s_device_state.screen_timeout_ms = timeout;
+  /** @todo Persist to Flash */
   k_mutex_unlock(&s_mux);
+
+  k_timer_start(&screen_timeout_timer, ms_to_timeout(timeout), K_NO_WAIT);
   return true;
+}
+
+void dev_state_kick_activity(void)
+{
+  k_mutex_lock(&s_mux, K_FOREVER);
+  uint32_t screen_ms = s_device_state.screen_timeout_ms;
+  uint32_t off_ms = s_device_state.off_screen_timeout_ms;
+  k_mutex_unlock(&s_mux);
+
+  // One-shot: no period, they get manually restarted on the next kick
+  k_timer_start(&screen_timeout_timer, ms_to_timeout(screen_ms), K_NO_WAIT);
+  k_timer_start(&off_timeout_timer, ms_to_timeout(off_ms), K_NO_WAIT);
+}
+
+/**
+ * @brief Called when it's time to go back to the menu screen
+ */
+static void screen_timeout_expiry(struct k_timer *timer_id)
+{
+  if (s_timeout_cb) { s_timeout_cb(DEV_STATE_TIMEOUT_SCREEN); }
+}
+
+/**
+ * @brief Called when it's time to power off the screen
+ */
+static void off_timeout_expiry(struct k_timer *timer_id)
+{
+  if (s_timeout_cb) { s_timeout_cb(DEV_STATE_TIMEOUT_PWR_OFF); }
 }
